97b227014d51ce03188ef6325edc58031ad6878c
jgables18 <31707093+Jgables18@users.noreply.github.com>
2018-02-09 12:39:02 -0600

    c
    c

---
+++ Close.py
@@ -0,0 +1,7 @@
+import RoboPiLib as RPL
+import setup
+
+RPL.servoWrite(0, # that is all the way Closed)
+RPL.servoWrite(1, # that is all the way Closed)
+
+Print 'Claw closed'
---
+++ Open.py
@@ -0,0 +1,5 @@
+import RoboPiLib as RPL
+import setup
+
+RPL.servoWrite(0, # that is all the way open)
+RPL.servoWrite(1, # that is all the way open)
