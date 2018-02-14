1d3fdc88b95c7245df16f6b9620c1dd774102edb
jgables18 <31707093+Jgables18@users.noreply.github.com>
2018-02-14 13:57:02 -0600

    ss
    ss

---
+++ Oak.py
@@ -0,0 +1,52 @@
+import RoboPiLib as RPL
+RPL.RoboPiInit("/dev/ttyAMA0",115200)
+
+import sys, tty, termios, signal
+
+def forward():
+  print "Forward"
+
+def reverse():
+  print "Reverse"
+
+def right():
+  print "Right"
+
+def left():
+  print "Left"
+
+fd = sys.stdin.fileno() # I don't know what this does
+old_settings = termios.tcgetattr(fd) # this records the existing console settings that are later changed with the tty.setraw... line so that they can be replaced when the loop ends
+
+######################################
+## Other motor commands should go here
+######################################
+
+def interrupted(signum, frame): # this is the method called at the end of the alarm
+  stopAll()
+
+signal.signal(signal.SIGALRM, interrupted) # this calls the 'interrupted' method when the alarm goes off
+tty.setraw(sys.stdin.fileno()) # this sets the style of the input
+
+print "Ready To Drive! Press * to quit.\r"
+## the SHORT_TIMEOUT needs to be greater than the press delay on your keyboard
+## on your computer, set the delay to 250 ms with `xset r rate 250 20`
+SHORT_TIMEOUT = 0.255 # number of seconds your want for timeout
+while True:
+  signal.setitimer(signal.ITIMER_REAL,SHORT_TIMEOUT) # this sets the alarm
+  ch = sys.stdin.read(1) # this reads one character of input without requiring an enter keypress
+  signal.setitimer(signal.ITIMER_REAL,0) # this turns off the alarm
+  if ch == '*': # pressing the asterisk key kills the process
+    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings) # this resets the console settings
+    break # this ends the loop
+  else:
+    if ch == 'w':
+      forward()
+    elif ch == "a":
+      left()
+    elif ch == "s":
+      reverse()
+    elif ch == "d":
+      right()
+    else:
+      stopAll()
